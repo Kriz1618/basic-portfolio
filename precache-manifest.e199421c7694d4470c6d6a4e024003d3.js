453d1b7c2ba71900a058eceb78c1a104cf78d4e3
Kriz <krizmarles@gmail.com>
2023-02-16 21:32:51 -0500

    Updates

---
+++ precache-manifest.e199421c7694d4470c6d6a4e024003d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc77b31668e8dfdfe6598acda1970944",
+    "url": "/basic-portfolio/index.html"
+  },
+  {
+    "revision": "c830abad31cb6f09027f",
+    "url": "/basic-portfolio/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "94874f7905ba62d28d34",
+    "url": "/basic-portfolio/static/css/main.f1dfb855.chunk.css"
+  },
+  {
+    "revision": "c830abad31cb6f09027f",
+    "url": "/basic-portfolio/static/js/2.b46c766d.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/basic-portfolio/static/js/2.b46c766d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94874f7905ba62d28d34",
+    "url": "/basic-portfolio/static/js/main.74f9b0e9.chunk.js"
+  },
+  {
+    "revision": "aea9bc9853e21288cdb1",
+    "url": "/basic-portfolio/static/js/runtime-main.a5802752.js"
+  },
+  {
+    "revision": "07027ce172545a93cda52285194f0a0d",
+    "url": "/basic-portfolio/static/media/person1.07027ce1.jpg"
+  }
+]);
